e6cd6d0993e7d9bb142fdb53d785f930ae586e7e
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-18 17:57:54 -0300

    feat(routes): added the functionalities (user creation and details of a user) in their respective routes

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
-
 /* Criando um novo objeto de rotas. */
 const router = express.Router();
+
+/* Controllers */
+const createUser = require("../controllers/User/createUser");
+const getUserById = require("../controllers/User/getUserById");
+
+/* Rotas de Usuários */
+router.post("/api/user", createUser);
+router.get("/api/user/:id", getUserById);
 
 module.exports = router;
