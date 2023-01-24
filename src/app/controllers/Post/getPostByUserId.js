499a662c7324e25e3873bf087cf02cf7357d39d6
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-24 18:41:19 -0300

    feat(getPostByUserId): added id validation received via params.

--- src/app/controllers/Post/getPostByUserId.js
+++ src/app/controllers/Post/getPostByUserId.js
@@ -3,26 +3,30 @@ const UserModel = require("../../models/UserModel");
 
 const getPostByUserId = (req, res) => {
   const id = req.params.id;
-  UserModel.findByPk(id)
-    .then((user) => {
-      const UserId = user.id;
-      if (UserId != undefined) {
-        PostModel.findAll({
-          where: {
-            UserId
-          }
-        })
-          .then((post) => {
-            return res.status(200).json(post);
+  if (isNaN(id)) {
+    return res.status(400).json({ message: "Invalid id" });
+  } else {
+    UserModel.findByPk(id)
+      .then((user) => {
+        const UserId = user.id;
+        if (UserId != undefined) {
+          PostModel.findAll({
+            where: {
+              UserId
+            }
           })
-          .catch((err) => {
-            return res.status(500).json(err);
-          });
-      } else {
-        return res.status(404).json({ message: "User not found" });
-      }
-    })
-    .catch((err) => res.send(err));
+            .then((post) => {
+              return res.status(200).json(post);
+            })
+            .catch((err) => {
+              return res.status(500).json(err);
+            });
+        } else {
+          return res.status(404).json({ message: "User not found" });
+        }
+      })
+      .catch((err) => res.send(err));
+  }
 };
 
 module.exports = getPostByUserId;
