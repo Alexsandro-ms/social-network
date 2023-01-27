89191ed1dacd9b72935e760cd45f84843e9d4544
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-27 16:40:46 -0300

    refactor(CreatePost): refactored post creation functionality.

--- src/app/controllers/Post/CreatePost.js
+++ src/app/controllers/Post/CreatePost.js
@@ -1,19 +1,22 @@
 const PostModel = require("../../models/PostModel");
 
 const createPost = (req, res) => {
-  const { title, body, userId } = req.body;
+  /* Desestruturando variáveis ( body e userId ) recebidas pelo corpo da requisição. */
+  const { body, userId } = req.body;
 
-  if (title != undefined && body != undefined && userId != undefined) {
-    PostModel.create({ title, body, UserId: userId })
-      .then((response) => {
-        return res.status(201).json({ message: "Post created!" });
-      })
-      .catch((err) => {
-        return res.status(500).json(err);
-      });
-  } else {
+  /* Verificando se o corpo e o userId não estão indefinidos. */
+  if (body === undefined || userId === undefined) {
     return res.status(400).json({ message: "Fill in all fields" });
   }
+
+  /* Criando um post e retornando uma mensagem se ela foi criada ou não. */
+  PostModel.create({ body, UserId: userId })
+    .then((response) => {
+      return res.status(201).json({ message: "Post created!" });
+    })
+    .catch((err) => {
+      return res.status(500).json(err);
+    });
 };
 
 module.exports = createPost;
