89771674146f47b8c9fa0832d5c4c8893751eee8
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-27 16:20:54 -0300

    refactor(AuthMiddleware): refactored middleware code and added new comments.

--- src/app/middlewares/AuthMiddleware.js
+++ src/app/middlewares/AuthMiddleware.js
@@ -1,28 +1,27 @@
 const jwt = require("jsonwebtoken");
+const JWTKEY = process.env.JWTKEY;
+const NOT_AUTHORIZED_MESSAGE = { message: "Not authorized!" };
 
 const AuthMiddleware = (req, res, next) => {
-  /* Obtendo o token do cabeçalho. */
-  const token = req.headers.authorization.split(" ")[1];
-
-  if (token != undefined) {
-    /* Obtendo o JWTKEY ​​do arquivo .env. */
-    const jwtKey = process.env.JWTKEY;
-
-    /* Verificando o token e se for válido, está retornando as informações armazenadas no payload. */
-    jwt.verify(token, jwtKey, (err, data) => {
-      if (err) {
-        return res.status(401).json({ message: "Not autorized!" });
-      } else {
-        req.token = token;
-        req.loggedUserInfo = {
-          name: data.name,
-          email: data.email
-        };
-      }
-      return next();
-    });
-  } else {
-    return res.status(401).json({ message: "Not authorized!" });
+  /* Desestruturando a propriedade de autorização do objeto req.headers. */
+  const { authorization } = req.headers;
+  /* Retornando um erro de não autorizado, caso não exista um cabeçalho de authorização. */
+  if (!authorization) {
+    return res.status(401).json(NOT_AUTHORIZED_MESSAGE);
+  }
+  try {
+    /* Atribuindo cabeçalho de autorização à variável chamada token. */
+    const token = authorization;
+    /* Desestruturando as propriedades de nome e e-mail do payload carregado no token. */
+    const { name, email } = jwt.verify(token, JWTKEY);
+    /* Atribuindo as variáveis ​​name e email à propriedade loggingUserInfo do objeto req. */
+    req.loggedUserInfo = { name, email };
+    /* Atribuindo token a propriedade token do objeto req, */
+    req.token = token;
+    return next();
+  } catch (err) {
+    /* Retornando status 401 ( Não autorizado. ) e um json contendo uma mensagem ( Not authorized! ) */
+    return res.status(401).json(NOT_AUTHORIZED_MESSAGE);
   }
 };
 
