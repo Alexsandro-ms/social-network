f4920d3854dd9421316708b6adedd95a7f31bd51
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-27 21:37:09 -0300

    feat(sendingEmailForgoutPassword): created the functionality that sends the email for password recovery.

---
+++ src/app/controllers/User/sendingEmailForgotPassword.js
@@ -0,0 +1,39 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const sendEmail = require("../../utils/sendEmail");
+const secret_key = process.env.JWTKEY;
+const UserModel = require("../../models/UserModel");
+
+const sendingEmailForgotPassword = async (req, res) => {
+  try {
+    /* Localizando o usuário com e-mail fornecido. */
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    /* Gerar token de redefinição de senha. */
+    const resetToken = jwt.sign({ email: user.email }, secret_key, {
+      expiresIn: "30s"
+    });
+    /* Atualizando senha do usuário com token de redefinição */
+    await UserModel.update(
+      { resetPasswordToken: resetToken },
+      { where: { id: user.id } }
+    );
+    /* Enviando e-mail com link de redefinição de senha. */
+    const htmlMessage = `
+    <p>Você solicitou uma redefinição de senha. Clique no link abaixo para redefinir sua senha:</p>
+    <button><a href="http://localhost:8080/api/user/forgout-password/${resetToken}">Redefinir senha</a></button>
+    `;
+    /* Enviando o email de redefinição de senha. */
+    await sendEmail(user.email, htmlMessage);
+    /* Enviando uma mensagem ao usuário informando que o e-mail de redefinição de senha foi enviado. */
+    return res.status(200).json({ message: "Password reset email sent" });
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .json({ error: "Error sending password reset email" });
+  }
+};
+module.exports = sendingEmailForgotPassword;
