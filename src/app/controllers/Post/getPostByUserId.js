c848ee80741ef0f6553c09095f1c730a339208b7
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-28 16:11:54 -0300

    refactor(getPostByUserId): refactored user post listing functionality.

--- src/app/controllers/Post/getPostByUserId.js
+++ src/app/controllers/Post/getPostByUserId.js
@@ -1,31 +1,31 @@
 const PostModel = require("../../models/PostModel");
 const UserModel = require("../../models/UserModel");
 
-const getPostByUserId = (req, res) => {
-  const id = req.params.id;
-  if (isNaN(id)) {
-    return res.status(400).json({ message: "Invalid id" });
-  } else {
-    UserModel.findByPk(id)
-      .then((user) => {
-        const UserId = user.id;
-        if (UserId != undefined) {
-          PostModel.findAll({
-            where: {
-              UserId
-            }
-          })
-            .then((post) => {
-              return res.status(200).json(post);
-            })
-            .catch((err) => {
-              return res.status(500).json(err);
-            });
-        } else {
-          return res.status(404).json({ message: "User not found" });
-        }
-      })
-      .catch((err) => res.send(err));
+const getPostByUserId = async (req, res) => {
+  try {
+    /* Recebendo id via parâmetros. */
+    const id = req.params.id;
+    /* Checando se o id é um número. */
+    if (isNaN(id)) {
+      return res.status(400).json({ message: "Invalid id." });
+    }
+
+    /* Procurando um usuário por id. */
+    const user = await UserModel.findByPk(id);
+    /* Caso não encontre, retornará um status 404 de usuário não encontrado. */
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    /* Procurando todos os posts apartit do id de usuário. */
+    const posts = await PostModel.findAll({
+      where: {
+        UserId: user.id
+      }
+    });
+    return res.status(200).json(posts);
+  } catch (error) {
+    return res.status(500).json(error);
   }
 };
 
