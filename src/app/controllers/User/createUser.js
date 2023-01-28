daab5e10c45b2e6cb41e2f3d9437d327d0d07d2f
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-27 21:40:03 -0300

    refactor(createUser): refactored user creation functionality

--- src/app/controllers/User/createUser.js
+++ src/app/controllers/User/createUser.js
@@ -1,40 +1,42 @@
 const UserModel = require("../../models/UserModel");
 const bcryt = require("bcrypt");
 
-const createUser = (req, res) => {
-  const { name, lastName, email, password } = req.body;
-  if (
-    name.length != 0 &&
-    lastName.length != 0 &&
-    email.length != 0 &&
-    password.length != 0
-  ) {
-    UserModel.findOne({
-      where: {
-        email
-      }
-    }).then((user) => {
-      const salt = bcryt.genSaltSync(10);
-      const hash = bcryt.hashSync(password, salt);
-      if (user == undefined) {
-        UserModel.create({
-          name,
-          lastName,
-          email,
-          password: hash
-        })
-          .then((user) => {
-            return res.status(201).json({ message: "User created!" });
-          })
-          .catch((err) => {
-            return res.status(500).json({ message: err });
-          });
-      } else {
-        return res.status(400).json({ message: "User already exists." });
-      }
+const createUser = async (req, res) => {
+  try {
+    /* Desestruturando variáveis recebidas no corpo da solicitação. */
+    const { name, lastName, email, password } = req.body;
+
+    /* Verificando se o usuário preencheu todos os campos. */
+    if (!name || !lastName || !email || !password) {
+      return res.status(400).json({ message: "Fill in all fields." });
+    }
+
+    /* Verificando se o usuário já existe no banco de dados. */
+    const existingUser = await UserModel.findOne({ where: { email } });
+
+    /* Se existir um usuário no banco de dados, retornará um status (400) e 
+    uma mensagem de usuário existente */
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists." });
+    }
+
+    /* Criptografando a senha. */
+    const salt = bcryt.genSaltSync(10);
+    const hash = bcryt.hashSync(password, salt);
+
+    /* Criando um novo usuário no banco de dados. */
+    await UserModel.create({
+      name,
+      lastName,
+      email,
+      password: hash
     });
-  } else {
-    return res.status(400).json({ message: "Fill in all fields." });
+
+    /* Retornando um código de status (201) e uma mensagem para o usuário. */
+    return res.status(201).json({ message: "User created!" });
+  } catch (err) {
+    /* Retornando um código de status (500) e uma mensagem para o usuário. */
+    return res.status(500).json({ message: err });
   }
 };
 
