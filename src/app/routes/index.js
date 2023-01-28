7cfe9d5118328a27708b0502e65d4317d9abd858
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-27 21:41:08 -0300

    feat(routes): added email sending functionality to change password and change user password in the application.

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -4,11 +4,9 @@ const express = require("express");
 const router = express.Router();
 
 /* Middlewares */
-
 const AuthMiddleware = require("../middlewares/AuthMiddleware");
 
 /* Controllers */
-
 /* Postes */
 const createPost = require("../controllers/Post/createPost");
 const getPostByUserId = require("../controllers/Post/getPostByUserId");
@@ -17,11 +15,15 @@ const getPostByUserId = require("../controllers/Post/getPostByUserId");
 const createUser = require("../controllers/User/createUser");
 const getUserById = require("../controllers/User/getUserById");
 const signInUser = require("../controllers/User/signinUser");
+const sendingEmailForgotPassword = require("../controllers/User/sendingEmailForgotPassword");
+const changePasswordUser = require("../controllers/User/changePasswordUser");
 
 /* Rotas de Usuários */
 router.post("/api/user", createUser);
 router.post("/api/user/signIn", signInUser);
 router.get("/api/user/:id", getUserById);
+router.post("/api/user/forgout-password", sendingEmailForgotPassword);
+router.patch("/api/user/forgout-password/:token", changePasswordUser);
 
 /* Rotas de postes */
 router.post("/api/post", AuthMiddleware, createPost);
