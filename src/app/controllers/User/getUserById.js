68b030eba0ccd3eaf156214dd5670a3209e2e664
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-27 21:39:28 -0300

    refactor(getUserById): refactored functionality that gets a user's data by id.

--- src/app/controllers/User/getUserById.js
+++ src/app/controllers/User/getUserById.js
@@ -1,16 +1,26 @@
 const UserModel = require("../../models/UserModel");
 
-const getUserById = (req, res) => {
-  if (isNaN(req.params.id)) {
-  } else {
+const getUserById = async (req, res) => {
+  try {
+    /* Verificando se o id é um número. */
+    if (isNaN(req.params.id)) {
+      /* Retornando código de status (400) e uma mensagem para usuário. */
+      return res.status(400).json({ message: "Invalid ID." });
+    }
+
+    /* Obtendo o usuário pelo id. */
     const id = req.params.id;
-    UserModel.findByPk(id)
-      .then((user) => {
-        return res.status(200).json(user);
-      })
-      .catch((err) => {
-        return res.status(500).json({ message: "User does not exist.", err });
-      });
+    const user = await UserModel.findByPk(id);
+
+    /* Se o usuário não for encontrado, retornará um código de status 404 e uma mensagem para o usuário. */
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    /* Retornando o objeto de usuário para o cliente. */
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500).json({ message: "An error occurred.", err });
   }
 };
 
