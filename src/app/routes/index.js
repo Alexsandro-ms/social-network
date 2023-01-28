50dd90cbad0e77d279d1b61d4dad52c089e77c17
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-28 15:56:11 -0300

    feat(routes): added functionality to delete user in the application and authorization middleware, so you can only remove it if you are logged in.

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -17,11 +17,13 @@ const getUserById = require("../controllers/User/getUserById");
 const signInUser = require("../controllers/User/signinUser");
 const sendingEmailForgotPassword = require("../controllers/User/sendingEmailForgotPassword");
 const changePasswordUser = require("../controllers/User/changePasswordUser");
+const deleteUser = require("../controllers/User/deleteUser");
 
 /* Rotas de Usuários */
 router.post("/api/user", createUser);
 router.post("/api/user/signIn", signInUser);
 router.get("/api/user/:id", getUserById);
+router.delete("/api/user", AuthMiddleware, deleteUser);
 router.post("/api/user/forgout-password", sendingEmailForgotPassword);
 router.patch("/api/user/forgout-password/:token", changePasswordUser);
 
