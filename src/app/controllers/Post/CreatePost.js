c290c5675a76cb4d065a185e18f6fe4ede525d13
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-02-01 16:16:21 -0300

    refactor(createPost): Refactored post creation resource.
    changes: 1. No longer get user id from body. 2. Use of async/await and try/catch.

--- src/app/controllers/Post/CreatePost.js
+++ src/app/controllers/Post/CreatePost.js
@@ -1,22 +1,23 @@
 const PostModel = require("../../models/PostModel");
 
-const createPost = (req, res) => {
-  /* Desestruturando variáveis ( body e userId ) recebidas pelo corpo da requisição. */
-  const { body, userId } = req.body;
+const createPost = async (req, res) => {
+  try {
+    /* Desetruturando body e userId. */
+    const { body } = req.body;
+    const { id: userId } = req.loggedUserInfo;
 
-  /* Verificando se o corpo e o userId não estão indefinidos. */
-  if (body === undefined || userId === undefined) {
-    return res.status(400).json({ message: "Fill in all fields" });
+    /* Verificando se o body e o userId são validos. */
+    if (!body || !userId) {
+      return res.status(400).json({ message: "Fill in all fields" });
+    }
+
+    /* Criando novo post. */
+    await PostModel.create({ body, UserId: userId });
+
+    return res.status(201).json({ message: "Post created!" });
+  } catch (err) {
+    return res.status(500).json(err);
   }
-
-  /* Criando um post e retornando uma mensagem se ela foi criada ou não. */
-  PostModel.create({ body, UserId: userId })
-    .then((response) => {
-      return res.status(201).json({ message: "Post created!" });
-    })
-    .catch((err) => {
-      return res.status(500).json(err);
-    });
 };
 
 module.exports = createPost;
