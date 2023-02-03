3ed68973028054c726887d8958c209bcfd0ffc9c
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-02-03 18:47:25 -0300

    feat(routes): configured and added middleware multer the application
    changes: 1. User will be able to upload an image (optional) on user creation.

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,7 +1,29 @@
 const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
 /* Criando um novo objeto de rotas. */
 const router = express.Router();
+
+/* Criando uma pasta chamada uploads no diretório raiz do projeto, caso não haja uma. */
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadsDir = path.resolve(__dirname, "..", "uploads");
+
+    if (!fs.existsSync(uploadsDir)) {
+      fs.mkdirSync(uploadsDir, { recursive: true });
+    }
+
+    cb(null, uploadsDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  }
+});
+
+/* Middleware de upload de imagem. */
+const upload = multer({ storage }).single("image");
 
 /* Middlewares */
 const AuthMiddleware = require("../middlewares/AuthMiddleware");
@@ -22,12 +44,13 @@ const unfollowUser = require("../controllers/Followers/unfollowUser");
 const createUser = require("../controllers/User/createUser");
 const getUserById = require("../controllers/User/getUserById");
 const signInUser = require("../controllers/User/signinUser");
-const sendingEmailForgotPassword = require("../controllers/User/sendingEmailForgotPassword");
-const changePasswordUser = require("../controllers/User/changePasswordUser");
 const deleteUser = require("../controllers/User/deleteUser");
 
+const changePasswordUser = require("../controllers/User/changePasswordUser");
+const sendingEmailForgotPassword = require("../controllers/User/sendingEmailForgotPassword");
+
 /* Rotas de Usuários */
-router.post("/api/user", createUser);
+router.post("/api/user", upload, createUser);
 router.post("/api/user/signIn", signInUser);
 router.get("/api/user/:id", getUserById);
 router.delete("/api/user", AuthMiddleware, deleteUser);
