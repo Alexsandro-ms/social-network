01b9cb5d76c3c0849daa6585f7d010ca5456f12a
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-02-17 14:51:04 -0300

    fix(createPost): fixed userId bug.
    UserId bug not saved in database fixed

--- src/controllers/Post/CreatePost.js
+++ src/controllers/Post/CreatePost.js
@@ -2,25 +2,27 @@ const PostModel = require("../../models/PostModel");
 
 const createPost = async (req, res) => {
   try {
-    /* Desetruturando body e userId. */
+    /* Destruturando o body, ID e name. */
     const { body } = req.body;
-    const { id: userId, name } = req.loggedUserInfo;
+    const { id, name } = req.loggedUserInfo;
 
-    /* Verificando se o body e o userId são validos. */
-    if (!name || !body || !userId) {
+    /* Verificando se o body, ID e name são válidos. */
+    if (!name || !body || !id) {
       return res.status(400).json({ message: "Fill in all fields" });
     }
 
+    /* Checando se existe um arquivo, associando o arquivo a variável image caso exista. */
     const image = req.file?.filename;
 
     /* Criando novo post. */
     await PostModel.create({
+      userId: id,
       userName: name,
       imagePath: image,
-      body,
-      UserId: userId
+      body
     });
 
+    /* Retornando um status code e uma mensagem para usuário. */
     return res.status(201).json({ message: "Post created!" });
   } catch (err) {
     return res.status(500).json(err);
