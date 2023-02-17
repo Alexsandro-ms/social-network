a3685977eeb5f06fa2e0045b4545d0c5668d2b78
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-02-17 14:21:26 -0300

    refactor(AuthMiddleware): Refactored authentication middleware.
    changes: added function to destructure schema and token.

--- src/middlewares/AuthMiddleware.js
+++ src/middlewares/AuthMiddleware.js
@@ -5,22 +5,28 @@ const NOT_AUTHORIZED_MESSAGE = { message: "Not authorized!" };
 const AuthMiddleware = (req, res, next) => {
   /* Desestruturando a propriedade de autorização do objeto req.headers. */
   const { authorization } = req.headers;
+
   /* Retornando um erro de não autorizado, caso não exista um cabeçalho de authorização. */
   if (!authorization) {
     return res.status(401).json(NOT_AUTHORIZED_MESSAGE);
   }
+
   try {
-    /* Atribuindo cabeçalho de autorização à variável chamada token. */
-    const token = authorization;
-    /* Desestruturando as propriedades de nome e e-mail do payload carregado no token. */
+    /* Separando o Bearer do token. */
+    const [scheme, token] = authorization.split(" ");
+
+    /* Verificando se o token é válido. */
     const { id, name, email } = jwt.verify(token, JWTKEY);
-    /* Atribuindo as variáveis ​​name e email à propriedade loggingUserInfo do objeto req. */
+
+    /* Atribuindo as variáveis name e email à propriedade loggingUserInfo do objeto req. */
     req.loggedUserInfo = { id, name, email };
-    /* Atribuindo token a propriedade token do objeto req, */
+
+    /* Atribuindo token a propriedade token do objeto req. */
     req.token = token;
+
     return next();
   } catch (err) {
-    /* Retornando status 401 ( Não autorizado. ) e um json contendo uma mensagem ( Not authorized! ) */
+    /* Retornando status 401 (Não autorizado) e um json contendo uma mensagem (Not authorized!). */
     return res.status(401).json(NOT_AUTHORIZED_MESSAGE);
   }
 };
